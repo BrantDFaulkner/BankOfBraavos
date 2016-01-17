132e9408d42dae4309fa98948cbad9279f32ce79
BrantDFaulkner <brantdfaulkner@gmail.com>
2016-01-17 14:54:08 -0600

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,7 +56,7 @@ members = [
 ]
 
 members.each { |member|
-  Member.new(user_name: member[0], rank: member[1], status: member[2])
+  Member.create!(user_name: member[0], rank: member[1], status: member[2])
   print "."
 }
 
