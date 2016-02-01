25aa2fd5e85078d60c1683701068f305c4b08d00
BrantDFaulkner <brantdfaulkner@gmail.com>
2016-01-31 19:32:03 -0600

    add war win percentage

--- app/controllers/wars_controller.rb
+++ app/controllers/wars_controller.rb
@@ -4,6 +4,10 @@ class WarsController < ApplicationController
 
   def index
     @wars = War.all.order(created_at: :desc)
+    wins = WarResult.find(2).wars.length
+    losses = WarResult.find(3).wars.length
+    @win_percentage = ((wins.to_f / (wins+losses)) * 100).round(2)
+
   end
 
   def show
--- app/models/war_result.rb
+++ app/models/war_result.rb
@@ -1,3 +1,4 @@
 class WarResult < ActiveRecord::Base
   has_many :wars
+
 end
